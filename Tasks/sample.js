5394d5811ae7f701de47a7e81642a32cbbd7efa0
daiyunjie <daiyunjie508@gmail.com>
2022-01-13 17:37:39 +0800

    fix

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,10 +1,7 @@
 // Get one random element from array
+'use strict';
 
-const SAMPLE = arr => {
-  arr = arr[
-    Math.floor(Math.random()* arr.length)
-  ]; return arr
-};
+const SAMPLE = arr => arr[Math.floor(Math.random()* arr.length)];
 
 const result = SAMPLE([123, 456, 789, 222, 333, 444]);
 console.log(result);
